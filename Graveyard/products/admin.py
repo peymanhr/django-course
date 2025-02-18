7e3e767bb03abfaa12f8257b477b197fafbbb425
peymanhr <phooshmand@gmail.com>
2025-02-18 00:39:20 +0000

    Create products admin.py
    python manage.py createsuperuser

--- Graveyard/products/admin.py
+++ Graveyard/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product
+from .models import VPN
+from .models import Giftcard
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "active", "created")
+    list_filter = ("active",)
+    search_fields = ("name",)
+    ordering = ("-created",)
+
+
+@admin.register(VPN)
+class VPNAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "type", "duration", "limit", "renewable", "active", "created")
+    list_filter = ("type", "renewable", "active")
+    search_fields = ("name", "type")
+    ordering = ("-created",)
+
+
+@admin.register(Giftcard)
+class GiftcardAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "type", "country", "active", "created")
+    list_filter = ("type", "country", "active")
+    search_fields = ("name", "type", "country")
+    ordering = ("-created",)
