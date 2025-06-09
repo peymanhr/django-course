bfb19f0250b717bd08a6df11e260f69eebe5cb06
peymanhr <phooshmand@gmail.com>
2025-06-09 17:16:36 +0000

    Form Handling

--- Graveyard/products/urls.py
+++ Graveyard/products/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('1', views.helloworld, name="one"),
     path('lezzat', views.alireza),
-    path('list', views.list),
+    path('list', views.list, name="list"),
+    path('form1', views.form1, name="form1"),
+    path('form1/<int:errno>', views.form1, name="form1"),
+    path('detail/<int:product_id>', views.detail, name="detail"),
+    path('delete/<int:product_id>', views.delete, name="delete"),
 ]
+ 
--- Graveyard/products/views.py
+++ Graveyard/products/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.template.loader import get_template
 from django.contrib.auth.decorators import login_required, permission_required
 
 from random import randint
 
+from .models import Product
 
 def helloworld(request):
     template = get_template('one.html')
@@ -29,6 +30,34 @@ def alireza(request):
                "products": products}
     return render(request, 'lezzat.html', context)
 
+
+def form1(request, errno=0):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if password != "ost":
+            return redirect('form1', errno=2)
+        # return HttpResponse(f"{username}:{password}")
+        return redirect('home')
+
+    elif request.method == 'GET':
+        context = {"errno": errno}
+        return render(request, 'form1.html', context)
+    
 def list(request):
-    context = {"err": "test1"}
-    return JsonResponse(context)
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'list.html', context)
+
+def detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {"product": product}
+    return render(request, 'detail.html', context)
+
+def delete(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product.delete()
+    return redirect('list')
+
+    
+    
